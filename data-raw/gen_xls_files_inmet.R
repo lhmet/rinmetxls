ab4b864e78fcce1f02f3aac9c9f51ff703460380
lhmet <lhmet@ufsm.br>
2018-10-20 10:49:15 -0300

    refactor: remove extdata, put data in vignettes, update vignette

--- data-raw/gen_xls_files_inmet.R
+++ data-raw/gen_xls_files_inmet.R
@@ -4,19 +4,24 @@ easypackages::libraries(c("here", "dplyr", "fs"))
 # fs::dir_delete()
 
 here()
-# script para gerar links para os arquivos xls das EMAs do INMET
+# script para gerar links para os arquivos xls das EMAs do INMET usados
+# na Vignette para mostrar organização dos dados
 fs::dir_create(here("inst/extdata", "dvd_xls_files"))
 
 # caminho para os arquivos brutos
-data_path <- "../inmetr_old/data/EMA_INMET/EMA_INMET_xls_ate_2017_Josue"
+data_path_rel <- "../inmetr_old/data/EMA_INMET/EMA_INMET_xls_ate_2017_Josue"
+fs::dir_exists(here::here(data_path))
+(data_path <- here::here(data_path_rel))
 
 fnames_raw <- fs::dir_ls(data_path,
   recursive = TRUE,
   glob = "*.xls"
 ) %>%
-  fs::path_abs()
+  path_abs()
 
-# construcao do caminho do link dos arquivos brutos para o extdata
+# construcao do caminho do link dos arquivos brutos para o vignettes
+# (antes estava copiando para o "extdata", mas estava demorando demais
+# para construir o pacote)
 # subdirs de regiao/estado
 sub_dirs <- fs::path_dir(fnames_raw) %>%
   fs::path_split() %>%
@@ -27,20 +32,21 @@ sub_dirs <- fs::path_dir(fnames_raw) %>%
   unlist() %>%
   fs::path()
 # caminhos dos diretorios destino com mesma estrutura do DVD
-path_extdata <- here::here("inst/extdata/dvd_xls_files") %>%
+#path_extdata <- here::here("inst/extdata/dvd_xls_files") %>%
+dest <- "vignettes/dvd_xls_files"
+path_dest <- here::here(dest) %>%
   fs::path(sub_dirs)
-fs::dir_create(path_extdata, recursive = TRUE)
-# caminhos para os arquivos que serão linkados no extdata
-fnames_extdata <- fs::path(path_extdata, fs::path_file(fnames_raw)) %>%
-  fs::path_abs()
+fs::dir_create(path_dest, recursive = TRUE)
+# caminhos para os arquivos que serão linkados no dest
+fnames_dest <- fs::path(path_dest, fs::path_file(fnames_raw))
 
-fs::link_create(fnames_raw, fnames_extdata)
-#fs::link_delete(fnames_extdata)
-dir_ls("inst/extdata/dvd_xls_files", recursive = TRUE)
-# link_delete(fnames_extdata)
+fs::link_create(fnames_raw, fnames_dest)
+#fs::link_delete(fnames_dest)
+dir_ls(dest, recursive = TRUE)
+# link_delete(fnames_dest)
 
 ## print files as characters
-# list.files("inst/extdata/dvd_xls_files", full.names = TRUE, recursive = TRUE)
+# list.files(dest, full.names = TRUE, recursive = TRUE)
 ## print files with colors
-# dir_ls(here("inst/extdata/dvd_xls_files"), recursive = TRUE)
+# dir_ls(here(dest), recursive = TRUE)
 
