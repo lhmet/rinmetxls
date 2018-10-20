d6e8f20ff6c8a981a475dc307ba907e78c172c49
lhmet <lhmet@ufsm.br>
2018-10-20 11:49:40 -0300

    chore: clean up comments

--- R/str_distinct.R
+++ R/str_distinct.R
@@ -11,16 +11,9 @@
 #' }
 #' @rdname str_distinct
 #' @export
-#' @importFrom data.table data.table
 str_distinct <- function(a, b) {
-  # x <- data.table::data.table(table(strsplit(a, "")[[1]]))
-  # y <- data.table::data.table(table(strsplit(b, "")[[1]]))
-  # dt <- y[x, on = "V1"][, N := ifelse(is.na(N), 0, N)][N != i.N, res := i.N - N][res > 0]
-  # rep(dt$V1, dt$res)
-
   y <- table(strsplit(b, "")[[1]])
   x <- table(strsplit(a, "")[[1]])
-
   substr(
     setdiff(
       x = paste0(names(x), x),
